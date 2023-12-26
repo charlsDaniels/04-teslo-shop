b690e3e77a27c80b1be4ed37eda9292bd8a10b66
Carlos Menendez <cdmenendez@teco.com.ar>
2023-12-26 10:07:22 -0300

    Changes on db config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Bootstrap');
   
   app.setGlobalPrefix('api')
   
@@ -18,7 +20,6 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
   
   await app.listen(port);
-
-  console.log(`App running on port: ${port}`)
+  logger.log(`App running on port ${ process.env.PORT }`);
 }
 bootstrap();
